ae544e6a44f34b0e3fc075d25d95742aba79056f
vladovdey <ovdevlad@gmail.com>
2019-03-12 19:08:46 +0300

    layout v0.1

--- js/common.js
+++ js/common.js
@@ -1,5 +1,17 @@
-$(function() {
+$(function () {
+	const switcherOn = document.querySelector('.on'),
+		switcherOff = document.querySelector('.off');
 
+	switcherOn.addEventListener('click', () => {
+		if (switcherOff.style.opacity == 0) {
+			switcherOff.style.opacity = '1';
+			switcherOn.style.opacity = '0';
+		}else{
+			switcherOff.style.opacity = '0';
+			switcherOn.style.opacity = '1';
+		}
+
+	});
 	// Custom JS
 
 });
